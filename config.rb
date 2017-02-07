161f6f3fd6e20da2dd5784100019552dc60a3ef6
haley akchurin <hdakchurin@gmail.com>
2017-02-07 14:55:28 -0600

    beautify

--- config.rb
+++ config.rb
@@ -2,12 +2,7 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-# http_path = "/"
-# css_dir = "stylesheets"
-# sass_dir = "sass"
-# images_dir = "assets"
-# javascripts_dir = "js"
-# fonts_dir = "fonts"
+
 
 http_path = "/"
 css_dir = "stylesheets"
